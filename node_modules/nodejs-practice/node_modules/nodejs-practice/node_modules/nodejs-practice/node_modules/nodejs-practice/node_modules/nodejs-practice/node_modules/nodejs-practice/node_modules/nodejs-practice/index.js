fe215e62c2f7b548a2368a8932d5de746e93ca0c
Vinayak Santhosh <18r01a04b3@cmritonline.ac.in>
2025-03-20 20:41:25 +0530

    node-js-practice-20-03-2025

--- node_modules/nodejs-practice/node_modules/nodejs-practice/node_modules/nodejs-practice/node_modules/nodejs-practice/node_modules/nodejs-practice/node_modules/nodejs-practice/node_modules/nodejs-practice/index.js
+++ node_modules/nodejs-practice/node_modules/nodejs-practice/node_modules/nodejs-practice/node_modules/nodejs-practice/node_modules/nodejs-practice/node_modules/nodejs-practice/node_modules/nodejs-practice/index.js
@@ -49,5 +49,5 @@ const server = http.createServer((req, res) => {
   else res.end("<h1>Thy Flesh Consumed</h1>");
 });
 server.listen(3000, "localhost", () => {
-  console.log("Server is running on http://localhost:3000");
+  console.log("Server is running http://localhost:3000");
 });
