5474bfa018a06ff9d147bea24297d5bcff4476fd
Rinzlertron456 <18r01a04b3@cmritonline.ac.in>
2025-06-28 21:46:07 +0530

    designed form component to implement RESTFul API in backend

--- index.js
+++ index.js
@@ -1,53 +1,9 @@
-//File-based module
-const word = {
-  letters: (word) => {
-    return word.split("").filter((char) => char !== " ");
-  },
-  firstLetter: (word) => {
-    return word.charAt(0);
-  },
-};
-module.exports = word;
+//RESTFul APIs using Node.js(Express.js)
+const express=require("express");
+const app=express();
+const port=3000;
+app.use(express.json());
+app.get("/",(req,res)=>{
+  res.send("Hello. Welcome to Home page");
+})
 
-//Build-in module
-//Asynchronous Method
-const fs = require("fs");
-const text = "Hello World";
-fs.writeFile("./example.txt", text, () => {
-  console.log("Written");
-});
-fs.readFile("./example.txt", "utf-8", (err, data) => {
-  if (err) return err;
-  console.log(data);
-});
-//Synchronous Method
-fs.writeFileSync("./sample.txt", text, () => {
-  console.log("Written");
-});
-fs.readFileSync("./example.txt", "utf-8", (err, data) => {
-  if (err) return err;
-  console.log(data);
-});
-const path = require("path");
-console.log(path.extname("./show.js"));
-console.log(path.basename("./show.js"));
-console.log(path.dirname("./show.js"));
-
-//OS related functions
-const os = require("os");
-console.log(os.platform);
-console.log(os.release);
-console.log(os.hostname);
-
-//Routing and http server configuration
-const http = require("http");
-const server = http.createServer((req, res) => {
-  if (req.url === "/doom-2016") res.end("Rip and Tear!! Until it's Done!!");
-  if (req.url === "/doom-eternal")
-    res.end("The Only Thing They Fear Is You!!!");
-  if (req.url === "/doom-the-dark-ages") res.end("Stand And Fight!!");
-  else res.end("<h1>Thy Flesh Consumed</h1>");
-});
-server.listen(3000, "localhost", () => {
-  console.log("Server is running http://localhost:3000");
-});
