3f61e7d71b40a2806d58738671d491bf5d432a55
Rinzlertron456 <18r01a04b3@cmritonline.ac.in>
2025-07-01 22:06:26 +0530

    formComponent completed, configuring axios post and nodejs http get request

--- form-api-frontend/src/pages/Form.jsx
+++ form-api-frontend/src/pages/Form.jsx
@@ -1,29 +1,35 @@
-import React from 'react'
-import { useRef } from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Form = () => {
-  const formRef=useRef();
+  const [data,setData] = useState({
+    email:'',
+    password:'',
+  });
+  const handleData=async(e)=>{
+    e.preventDefault();
+    console.log(data);
+    await axios.post('http://localhost:3000/form',data);
+  }
   return (
     <>
-        {/* <div className="form-container"> */}
-            <form>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            <form className='form-container' onSubmit={handleData}>
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value={data.email} onChange={(e)=>setData({...data,email:e.target.value})}/>
+                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                <div className="form-group">
+                    <label htmlFor="exampleInputPassword1">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" value={data.password} onChange={(e)=>setData({...data,password:e.target.value})}/>
                 </div>
-                <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                <div className="form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
-        {/* </div> */}
     </>
   )
 }
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 //RESTFul APIs using Node.js(Express.js)
-const express=require("express");
+import express, { json } from "express";
 const app=express();
 const port=3000;
-app.use(express.json());
+app.use(json());
 app.get("/",(req,res)=>{
   res.send("Hello. Welcome to Home page");
+})
+app.get("/form",(req,res)=>{
+  console.log(req.body);
+  res.send(req.body);
+})
+
+app.listen(port,()=>{
+  console.log(`Server is running on http://localhost:${port}`);
 })
 
