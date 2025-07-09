06e41bc0853721522f970598407bd1e47b35147c
Rinzlertron456 <18r01a04b3@cmritonline.ac.in>
2025-07-09 20:44:44 +0530

    successfully implemented mysql queries for creating table and inserting data into tables

--- index.js
+++ index.js
@@ -18,8 +18,14 @@ const connection = createPool({
 app.get("/",(req,res)=>{
   res.send("Hello. Welcome to Home page");
 })
-app.post("/form",(req,res)=>{
+app.post("/form",(req,res)=>{ 
   console.log(req.body);
+  connection.query("CREATE TABLE IF NOT EXISTS dummydata ( email varchar(45), password varchar(45))",(error,results)=>{
+    if(error){
+      console.log("Error creating table",error);
+    }
+    return;
+  })
   connection.query("INSERT INTO dummydata (email, password) VALUES (?, ?)", [req.body.email, req.body.password], (error, results) => {
     if (error) {
       console.error("Error inserting data:", error);
@@ -28,6 +34,11 @@ app.post("/form",(req,res)=>{
     console.log("Data inserted successfully:", results);
   });
   res.send(req.body);
+})
+
+app.get("/user",(req,res)=>{
+  console.log(req.body);
+  
 })
 
 app.listen(port,()=>{
